cd7f77f7a835911037af992c3e602713f5846071
@2513110292-kita <2513110292@tni.ac.th>
2026-07-20 19:19:32 +0700

    2513110292 commit LabSheet03

--- LabSheet03/src/LinkedList.java
+++ LabSheet03/src/LinkedList.java
@@ -1,8 +1,8 @@
 public class LinkedList {
-    Node head; 
+    private Node head;
 
     public LinkedList() {
-        this.head = null; 
+        this.head = null;
     }
 
     public String traversal() {
@@ -10,12 +10,9 @@ public class LinkedList {
         String result = "[";
         boolean first = true;
         while (current_node != null) {
-            if (!first) {
-                result += ", ";
-            }
-            result += current_node.data;
-            first = false;
+            result += (!first ? ", " : "") + current_node.data;
             current_node = current_node.next;
+            first = false;
         }
         result += "]";
         return result;
@@ -23,14 +20,16 @@ public class LinkedList {
 
     public void insert(int position, Object value) {
         Node new_node = new Node(value);
-        
-        if (position == 0 || head == null) {
+
+        if (head == null) {
+            head = new_node;
+        } else if (position == 0) {
             new_node.next = head;
             head = new_node;
         } else {
             Node current_node = head;
-            int current_position = 0;
-            while (current_node.next != null && current_position < position - 1) {
+            int  current_position = 0;
+            while (current_node != null && current_position < position - 1) {
                 current_node = current_node.next;
                 current_position++;
             }
@@ -38,9 +37,10 @@ public class LinkedList {
             current_node.next = new_node;
         }
     }
- 
+
     public void insert(Object value) {
         Node new_node = new Node(value);
+
         if (head == null) {
             head = new_node;
         } else {
@@ -51,47 +51,73 @@ public class LinkedList {
             current_node.next = new_node;
         }
     }
+
     public void remove(int position) {
         if (position == 0) {
-        	Node current_node = head;
+            Node current_node = head;
             head = current_node.next;
-          } else {
-                    Node current_node = head;
-                    int current_position = 0;
-                    
-                    while (current_node.next != null && current_position < position - 1) {
-                        current_node = current_node.next;
-                        current_position++;
-                    }
-                        current_node.next = current_node.next.next;
-                    }
-                }
-    public void removeLastElement() {
-    	if (head == null);
-    	if (head.next == null) {
-            head = null;
-            return;
+        } else {
+            Node current_node = head;
+            int  current_position = 0;
+            while (current_node.next.next != null && current_position < position - 1) {
+                current_node = current_node.next;
+                current_position++;
+            }
+            current_node.next = current_node.next.next;
         }
-        Node current = head;
-        while (current.next.next != null) {
-            current = current.next;
-        }
-        current.next = null;
-    	}
     }
-	
-	public int lenght() {
-		return 0;
-	}
-	
-	public Object get(int position) {
-		return null;
-	}
-	
-	public void set(int position,Object value) {
-		
-	}
-	
-	public void clear() {
-		head = null;
-	}
+
+    public void removeLastElement() {
+        if (head != null) {
+            if (head.next == null) {
+                head = null;
+            } else {
+                Node current_node = head;
+                while (current_node.next.next != null) {
+                    current_node = current_node.next;
+                }
+                current_node.next = null;
+            }
+        }
+    }
+
+    public int length() {
+        int  count = 0;
+        Node current_node = head;
+        while (current_node != null) {
+            count++;
+            current_node = current_node.next;
+        }
+        return count;
+    }
+
+    public Object get(int position) {
+        Node current_node = head;
+        int  current_position = 0;
+        while (current_node != null) {
+            if (current_position == position) {
+                return current_node.data;
+            }
+            current_node = current_node.next;
+            current_position++;
+        }
+        return null;
+    }
+
+    public void set(int position, Object value) {
+        Node current_node = head;
+        int  current_position = 0;
+        while (current_node != null) {
+            if (current_position == position) {
+                current_node.data = value;
+                return;
+            }
+            current_node = current_node.next;
+            current_position++;
+        }
+    }
+
+    public void clear() {
+        head = null;
+    }
+}
